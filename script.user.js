3f92b24b89f3562eb50b3af588de9ea804fcc7cf
NecRaul <necraul2001@gmail.com>
2023-09-02 21:14:00 +0400

    Initial commit
    Script to hide/filter the Isekai general on /a/

---
+++ script.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Hide Isekai general on /a/
+// @version      1.0
+// @description  Hide the isekai general on /a/ recognizable by it's OP post containing the word isekai in spoiler.
+// @author       https://github.com/NecRaul
+// @match        https://boards.4channel.org/a/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=4channel.org
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  document.addEventListener(
+    "PostsInserted", // uses the 4chanX API
+    function () {
+      // Find all elements with the class "catalog-thread"
+      const catalogThreads = document.querySelectorAll(".catalog-thread");
+      // Iterate through each catalog thread
+      catalogThreads.forEach((thread) => {
+        // Find the blockquote element within the current catalog thread
+        const blockquote = thread.querySelector(
+          ".catalog-container .catalog-post blockquote.postMessage"
+        );
+        // Check if the blockquote element exists and its innerHTML contains "<s>isekai</s>"
+        // Replace this with something else if you want block that
+        if (blockquote && blockquote.innerHTML.includes("<s>isekai</s>")) {
+          // Apply CSS style to hide the catalog thread
+          thread.style.display = "none";
+        }
+      });
+    },
+    false
+  );
+})();
